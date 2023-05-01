a5fb0be23b81b376be2d26c0909cd24eca524c12
painaddict <115467129+painaddict@users.noreply.github.com>
2023-05-02 04:57:18 +0530

    added config base

---
+++ config_example.py
@@ -0,0 +1,25 @@
+COOKIES = {
+
+}
+
+HEADERS = {
+    'authority': 'www.linkedin.com',
+    'referer': 'https://www.linkedin.com/learning/me/my-library/in-progress',
+    'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+    'csrf-token': 'ajax:0253577805716565985',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Chrome OS"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+    'x-li-identity': 'dXJuOmxpOmVudGVycHJpc2VQcm9maWxlOih1cm46bGk6ZW50ZXJwcmlzZUFjY291bnQ6OTI5NjE2OTIsMTgwODkwMDM3KQ==',
+    'x-li-lang': 'en_US',
+    'x-li-page-instance': 'urn:li:page:d_learning_paths;omu49EG6SYyOe5WnoDQxYg==',
+    'x-li-pem-metadata': 'Learning Exp - Content=learning-path',
+    'x-li-track': '{"clientVersion":"1.1.3025","mpVersion":"1.1.3025","osName":"web","timezoneOffset":5.5,"timezone":"Asia/Calcutta","mpName":"learning-web","epApp":"learning","displayDensity":1.25,"displayWidth":1920,"displayHeight":1080}',
+    'x-lil-intl-library': 'en_US',
+    'x-restli-protocol-version': '2.0.0',
+}
+
+BASE_URL = "https://www.linkedin.com/learning-api/"
