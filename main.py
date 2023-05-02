5fe8a4540fe0f73aedb9c1cbd3392aed2a068b7e
painaddict <115467129+painaddict@users.noreply.github.com>
2023-05-02 11:54:57 +0530

    added deco parsing

--- main.py
+++ main.py
@@ -33,11 +33,32 @@ class Linkedout(object):
 			sys.exit(1)
 	
 	def parse_course(self):
+		if '404' in json.dumps(self.course_json):
+			logger.error("Improper slug!")
+			sys.exit(1)
+
 		self.course_title = self.course_json['data']['elements'][0]['title']
 		self.duration = str(datetime.timedelta(seconds = self.course_json['data']['elements'][0]['contentDurationInSeconds']))
 		logger.info("Course Name: " + self.course_title)
 		logger.info("Duration: " + self.duration)
 
+		for card in self.course_json['included']:
+			if card['$type'] == 'com.linkedin.learning.api.common.Card':
+				logger.info('Course found: ' + card['headline']['title']['text'] + ' -- ' +  
+							str(datetime.timedelta(seconds = card['length']['duration'])))
+				self.get_deco_details(card['slug'])
+
+	def get_deco_details(self, slug):
+		deco_json = self.session.get(url=config.BASE_URL + 'courses', params={
+				'decorationId': 'com.linkedin.learning.api.deco.content.DecoratedCourse-96',
+				'q': 'slug',
+				'slug': slug
+			}, headers={
+				'accept': 'application/vnd.linkedin.normalized+json+2.1'
+			}).json()
+		for video in deco_json['included']:
+			if video['$type'] == 'com.linkedin.learning.api.deco.content.Video':
+				logger.debug('Watching ' + video['title'])
 
 @logger.catch
 def main():
